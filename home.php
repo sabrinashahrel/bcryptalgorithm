3709f634a7b5bb88313f5a1dccc50b674c364847
sabrinashahrel <79493431+sabrinashahrel@users.noreply.github.com>
2021-11-21 22:35:20 +0800

    Add files via upload

---
+++ home.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+  background-image: url('floral.jpg');
+    background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: 100% 100%;
+ 
+}
+</style>
+<meta charset="utf-8">
+<title>Homepage</title>
+<link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+
+<h1 style="text-align:center; font-size:250%; font-family:verdana"><b>Welcome to Online Diary</b></h1>
+<p style="text-align:center; font-size:100%; font-family: Lucida Console, Courier New, monospace;" ><strong><i>Keep your personal memory secretly</i></strong></p><br>
+
+
+<div class="form">
+
+<p><a style="font-size:150%;  border: 2px solid Violet;" href="setFolder.php">Create New Folder</a></p>
+<a style="font-size:150%; border: 2px solid Violet;" href="diarypin.php">My Diary Collection</a></p>
+
+
+
+
+
+</div>
+</body>
+</html>
